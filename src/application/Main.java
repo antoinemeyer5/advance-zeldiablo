e0c427f6c5bff32faaa88fc58f1b36862022b594
Antoine Meyer <antoine.meyer7@etu.univ-lorraine.fr>
2020-10-26 22:12:53 +0100

    Ajout du dossier src contenant tout le code java

---
+++ src/application/Main.java
@@ -0,0 +1,25 @@
+package application;
+
+import moteurJeu.DessinJeu;
+import moteurJeu.Jeu;
+import moteurJeu.MoteurGraphique;
+
+/**
+ * classe principale, c'est le coeur du jeu
+ * @author geronimus, grimmer, gross et meyer
+ */
+public class Main {   
+
+	public static void main(String[] args)throws InterruptedException {
+		
+		// creation du jeu particulier et de son afficheur
+		Jeu jeu = new JeuLabyrinthe();
+		DessinJeu aff = new DessinLabyrinthe((JeuLabyrinthe) jeu);
+		
+		// classe qui lance le moteur de jeu generique
+		MoteurGraphique moteur = new MoteurGraphique(jeu, aff);
+		
+		// lance la boucle de jeu qui tourne jusque la fin du jeu
+		moteur.lancerJeu(440, 490);		
+	}
+}
---
+++ src/application/Potion.java
@@ -0,0 +1,16 @@
+
+package application;
+
+public class Potion extends Objet {
+
+    /**
+     * Instancie une amulette a partir du constructeur d'objets
+     * @param dx abscisse de l'amulette
+     * @param dy ordonnee de l'amulette
+     */
+    public Potion(int dx, int dy) {
+
+        super(dx, dy);
+    }
+
+}
